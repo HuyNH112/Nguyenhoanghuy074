ba360ac688d6fa0eff3be965eb95deec90499956
HuyNH112 <hoanghuyn71@gmail.com>
2025-07-16 10:28:19 +0700

    Sources Game and Sources Code

---
+++ Assets/Scipts/CharacterSelect.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; 
+
+public class CharacterSelect : MonoBehaviour
+{
+    private int index;
+    [SerializeField] private GameObject[] characters; 
+    [SerializeField] TextMeshProUGUI characterName; 
+    [SerializeField] GameObject[] characterPrefabs; 
+    public static GameObject selectedCharacter; 
+    public Button backButton; 
+    private const string SelectedCharacterIndexKey = "SelectedCharacterIndex";
+
+    void Start()
+    {
+        
+        index = PlayerPrefs.GetInt(SelectedCharacterIndexKey, 0);
+
+        if (index >= characters.Length || index < 0)
+        {
+            index = 0;
+        }
+        SelectCharacter(); 
+
+        if (backButton != null)
+        {
+            backButton.onClick.RemoveAllListeners(); 
+            backButton.onClick.AddListener(OnBackButtonClick); 
+        }
+        else
+        {
+            Debug.LogError("Back Button is not assigned in CharacterSelect!");
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            OnNextButtonClick();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            OnPreviousButtonClick(); 
+        }
+        else if (Input.GetKeyDown(KeyCode.Return)) 
+        {
+            OnPlayButtonClick(); 
+        }
+    }
+    public void OnPlayButtonClick()
+    {
+        
+        PlayerPrefs.SetInt(SelectedCharacterIndexKey, index);
+        PlayerPrefs.Save(); 
+
+        
+        SceneManager.LoadScene("GamePlay");
+    }
+    public void OnPreviousButtonClick()
+    {
+        index--; 
+
+        if (index < 0)
+        {
+            index = characters.Length - 1;
+        }
+        SelectCharacter();
+        Debug.Log("Selected character index: " + index);
+    }
+    public void OnNextButtonClick()
+    {
+        index++; 
+    
+        if (index >= characters.Length) 
+        {
+            index = 0;
+        }
+        SelectCharacter();
+        Debug.Log("Selected character index: " + index);
+    }
+    private void SelectCharacter()
+    {
+        for (int i = 0; i < characters.Length; i++)
+        {
+            SpriteRenderer characterSprite = characters[i].GetComponent<SpriteRenderer>();
+
+            if (i == index)
+            {
+                characterSprite.color = Color.white;
+                selectedCharacter = characterPrefabs[i]; 
+                characterName.text = characterPrefabs[i].name;  
+            }
+            else
+            {              
+                characterSprite.color = Color.grey; 
+            }
+        }
+    }
+    public void OnBackButtonClick()
+    {
+        SceneManager.LoadScene("MainMenu"); 
+    }
+}
---
+++ Assets/Scipts/PipeMoveScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class pipeMoveScript : MovableObjectBase
+{
+    public float deadZone = -30;
+    // Vertical movement properties
+    public float verticalMoveAmplitude = 2f; 
+    public float verticalMoveFrequency = 1f; 
+    private float verticalMoveOffset;
+    // Rotation properties
+    public float rotationAmplitude = 15f; 
+    public float rotationFrequency = 1.5f; 
+    private float rotationOffset;
+
+    void Start()
+    {
+        verticalMoveOffset = Random.Range(0f, 10f);
+        rotationOffset = Random.Range(0f, 10f);
+    }
+    
+    void Update()
+    {
+        
+        if (LogicScript.instance != null && LogicScript.instance.gameHasStarted && !LogicScript.instance.isGameOver)
+        { 
+            MoveStep(Time.deltaTime);
+
+            if (LogicScript.instance.Score >= 10)
+            {
+                float verticalMovement = Mathf.Sin(Time.time * verticalMoveFrequency + verticalMoveOffset) * verticalMoveAmplitude;
+                transform.position = new Vector3(transform.position.x, transform.position.y + verticalMovement * Time.deltaTime, transform.position.z);
+            }
+
+            if (LogicScript.instance.Score >= 20)
+            {
+                
+                float currentRotationZ = Mathf.Sin(Time.time * rotationFrequency + rotationOffset) * rotationAmplitude;
+                transform.rotation = Quaternion.Euler(0, 0, currentRotationZ);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+        }
+
+        if (transform.position.x < deadZone)
+        {
+            Destroy(gameObject); 
+        }
+    }
+}
